d616117f2d34574594f4100191c14143cb1d9aa9
MANIKANDAN C <manikandan46chandran@gmail.com>
2026-02-02 20:26:34 +0530

    Update database password for DBConnection

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/mini_bank";
     private static final String USER = "root";
-    private static final String PASSWORD = "vomala";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
